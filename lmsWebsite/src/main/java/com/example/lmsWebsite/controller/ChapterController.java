5d98b0b409877ea0437378d7c2829f7a6a6fd88f
khushidayaramani1 <khushi.dayaramani1@gmail.com>
2026-02-19 21:28:43 +0530

    removed api key

--- lmsWebsite/src/main/java/com/example/lmsWebsite/controller/ChapterController.java
+++ lmsWebsite/src/main/java/com/example/lmsWebsite/controller/ChapterController.java
@@ -6,14 +6,14 @@ import com.example.lmsWebsite.service.ChapterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;    
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","https://new-lms-website-jud2.vercel.app/"})
 public class ChapterController {
 
     @Autowired
